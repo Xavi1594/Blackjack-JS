b7fd4c189e7a1c96954696f7d35db06984d447da
Xavi1594 <xavi9415@hotmail.com>
2022-12-28 14:21:54 +0100

    funcionalidad de pedir carta

--- Assets/js/juego.js
+++ Assets/js/juego.js
@@ -4,9 +4,10 @@ const tipos = ['C','D','H','S' ]
 const cartasEspeciales = ['A','J','Q','K']
 
 const crearDeck = () => {
+
 for (let i = 2; i <= 10; i++) {
-    for ( tipo of tipos) {
-        deck.push( i + 'tipo');
+    for ( let tipo of tipos) {
+        deck.push( i + tipo);
     }
 }
 for (let tipo of tipos) {
@@ -20,3 +21,18 @@ console.log(deck);
 return deck;
 }
 crearDeck();
+
+const pedirCarta = () => {
+    if (deck.length=== 0) {
+        throw 'No hay cartas en el deck'
+        
+    }
+
+  const carta = deck.pop()
+  console.log(deck);
+  console.log(carta);
+  return carta;
+}
+
+pedirCarta()
+
