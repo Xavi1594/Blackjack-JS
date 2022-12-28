711fec44541611e57075b62de819e848c1471723
Xavi1594 <xavi9415@hotmail.com>
2022-12-28 13:48:51 +0100

    baraja creada

--- Assets/js/juego.js
+++ Assets/js/juego.js
@@ -7,11 +7,16 @@ const crearDeck = () => {
 for (let i = 2; i <= 10; i++) {
     for ( tipo of tipos) {
         deck.push( i + 'tipo');
-        
     }
-
-
+}
+for (let tipo of tipos) {
+    for (let esp of cartasEspeciales) {
+        deck.push(esp + tipo);
+    }
 }
 console.log(deck);
+deck = _.shuffle( deck );
+console.log(deck);
+return deck;
 }
-crearDeck()
+crearDeck();
